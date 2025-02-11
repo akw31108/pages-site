0bab5f7909f1be214552a6723dbe098c12b9acf8
Amber Woolam <akw31108@email.vccs.edu>
2025-02-11 13:28:05 -0800

    Added docs to repository

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+    base: "/pages-site/",
     build: {
         outDir: "docs",
         rollupOptions: {
             input: {
-                main: resolve(__dirname, "./index.html"),
-                about: resolve(__dirname, "./about/index.html"),
-                california: resolve(__dirname, "./california/index.html"),
+                main: resolve(__dirname, "index.html"),
+                about: resolve(__dirname, "about/index.html"),
+                california: resolve(__dirname, "california/index.html"),
                 family: resolve(__dirname, "./family/index.html"),
             },
         },
