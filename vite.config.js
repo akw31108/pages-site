3b2255f60d07ca2c2fd2000efd3216b0964ef9d8
Amber Woolam <akw31108@email.vccs.edu>
2025-02-11 12:37:49 -0800

    trying to add links to about, california, and family pages

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
         outDir: "docs",
         rollupOptions: {
             input: {
-                main: resolve(__dirname, "index.html"),
-                about: resolve(__dirname, "about/index.html"),
-                california: resolve(__dirname, "california/index.html"),
+                main: resolve(__dirname, "./index.html"),
+                about: resolve(__dirname, "./about/index.html"),
+                california: resolve(__dirname, "./california/index.html"),
                 family: resolve(__dirname, "./family/index.html"),
             },
         },
