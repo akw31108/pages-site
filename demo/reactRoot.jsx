ca9dc35d4d8b43bc412f57270d1c29558f158928
Amber Woolam <akw31108@email.vccs.edu>
2025-02-25 17:30:23 -0800

    added todoList

--- demo/reactRoot.jsx
+++ demo/reactRoot.jsx
@@ -1,4 +1,4 @@
-import { createRoot} from "react-dom/client";
+import {createRoot} from "react-dom/client";
 
 const root = createRoot(document.querySelector("main"));
 root.render(<h1>Hello, world</h1>);
